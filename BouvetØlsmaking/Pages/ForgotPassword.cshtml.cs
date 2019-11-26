e184966aa08b0c75521d8d8af1cf2b961e7432e3
Andreas Skaret <andreas.skaret@gmail.com>
2019-11-26 19:24:27 +0100

    Updated Youtube video for 2019

--- BouvetØlsmaking/Pages/ForgotPassword.cshtml.cs
+++ BouvetØlsmaking/Pages/ForgotPassword.cshtml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BouvetØlsmaking.Models;
+﻿using BouvetØlsmaking.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BouvetØlsmaking.Pages
 {
